9ec14cc17b85a5e854b1d537646a0678cb810f05
abdelghanyMh <wboductqr@relay.firefox.com>
2022-10-29 14:43:30 +0100

    add: modal helper

---
+++ app/Helpers/ModelHelpers.php
@@ -0,0 +1,17 @@
+<?php
+ 
+namespace App\Helpers;
+use App\Models\Patient;
+use App\Models\User;
+ 
+class ModelHelpers {
+
+    //  The method inserts a record in the doctor-patient relationship intermediate table t
+    // to create the link between a patient and the user (with doctor role).
+ 
+    public static function attachPatient($doctorID,$patientID)
+    {
+        $doctor = User::find($doctorID);
+        $doctor->patients()->sync($patientID);
+    }
+}
--- config/app.php
+++ config/app.php
@@ -211,6 +211,7 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'ExampleClass' => App\Example\ExampleClass::class,
+        'ModelHelpers' => App\Helpers\ModelHelpers::class,
     ])->toArray(),
 
 ];
